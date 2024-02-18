37a2c84d3015ebde7848f066a77e03177a002810
victorbamikole <victorbamikole92@gmail.com>
2024-02-18 02:23:41 -0800

    Delete all endpoint added

--- index.js
+++ index.js
@@ -77,14 +77,6 @@ app.patch("/jokes/:id", (req, res) => {
 //7. DELETE Specific joke
 app.delete("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  let newArray = jokes.filter((item) => item.id !== id);
-  console.log(newArray);
-  res.sendStatus(200);
-});
-
-//8. DELETE All jokes
-app.delete("/jokes/:id", (req, res) => {
-  const id = parseInt(req.params.id);
   const searchIndex = jokes.findIndex((joke) => joke.id === id);
   if (searchIndex > -1) {
     jokes.splice(searchIndex, 1);
@@ -93,6 +85,18 @@ app.delete("/jokes/:id", (req, res) => {
     res
       .status(404)
       .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
+//8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
   }
 });
 
